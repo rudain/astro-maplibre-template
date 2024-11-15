b1dadd3e0f4f924581ec9694ee1613d216ee97b3
rudain <rudainalm@gmail.com>
2024-11-14 19:30:15 -0500

    url change

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://github.com/rudain",
+  site: "https://rudain.github.io",
   base: "astro-maplibre-template",
   integrations: [mdx(), sitemap(), tailwind()],
 });
